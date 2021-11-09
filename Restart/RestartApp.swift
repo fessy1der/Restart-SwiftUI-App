b2c1c8a7fec2f3e5ea380e83f36c854e1851dcdb
Festus obi <fessy_wonder@yahoo.co.uk>
2021-11-09 18:27:36 +0100

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Festus Obi on 09/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
